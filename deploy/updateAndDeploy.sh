270eb133d6ffd955a1a8a270c5243e86c983d7fd
akashbabu256 <001akashbabu@gmail.com>
2017-05-21 19:03:51 +0530

    Initial Commit

---
+++ deploy/updateAndDeploy.sh
@@ -0,0 +1,5 @@
+# Please Enter the path for you Project
+cd ""
+git pull origin master
+npm start
+echo "\n*********Successfully Deployed*************\n"
---
+++ deploy/webHookServer.js
@@ -0,0 +1,88 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var config = require("../config/config")
+
+var port = config.deploy.webhook.port
+
+var app = express()
+
+app.use(bodyParser.json({
+    limit: '1mb'
+}))
+
+app.post("/webhook", function(req, res) {
+    if (req.headers['content-type'] != 'application/json') {
+        return res.status(400).send("Please select content-type as application/json");
+    }
+    var event = getEvent(req)
+
+    if (event == config.deploy.webhook.event) {
+        if (req.headers['x-hub-signature']) {
+            var crypto = require('crypto')
+            var hmac = crypto.createHmac('sha1', deploy.webhook.secret)
+            var computedSignature = hmac.update(Buffer.from(JSON.stringify(req.body), 'utf-8'), 'utf-8').digest('hex')
+            if (req.headers['x-hub-signature'].split("=")[1] == computedSignature) {
+                var execFile = require("child_process").execFile;
+                execFile("./updateAndDeploy.sh", (err, stdout, stderr) => {
+                    if (!err) {
+                        console.log('stdout:', stdout)
+                        console.log('stderr:', stderr)
+                    } else {
+                        console.error('Failed to run updateAndDeploy.sh script')
+                    }
+                    res.status(200).send("Success");
+                })
+            } else {
+                res.status(400).send();
+            }
+        } else {
+            res.status(400).send("x-hub-signature header is absent")
+        }
+    } else {
+        res.status(400).send("Not The Required Event");
+    }
+})
+
+/**
+ * Return the github Event from the Request Object
+ * It returns event in the form of event:reponame:ref
+ * 
+ * @returns {string}
+ */
+function getEvent(req) {
+    var type = req.headers['x-github-event']
+    var reponame = ""
+    var ref = ""
+    if (req.body) {
+        if (req.body.repository) {
+            if (req.body.repository.name) {
+                reponame = req.body.repository.name;
+            } else {
+                errLog.error('Undefined req.body.repository.name');
+                errLog.error('body:', req.body);
+            }
+        } else {
+            errLog.error('Undefined req.body.repository');
+            errLog.error('body:', req.body);
+        }
+
+        if (req.body.ref) {
+            ref = req.body.ref;
+        } else {
+            errLog.error('Undefined req.body.ref');
+            errLog.error('body:', req.body);
+        }
+    } else {
+        errLog.error('Undefined req.body');
+    }
+
+    return type + ":" + reponame + ":" + ref
+}
+
+app.listen(port, (err) => {
+    if (!err) console.log('Git WebHook Server/Receiver Started on port:', port)
+})
+
+process.on("uncaughtException", (err) => {
+    console.error('UncaughtException:', err)
+})
